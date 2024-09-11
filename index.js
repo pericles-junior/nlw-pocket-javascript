7e71596b897e010e5a2f53d7c814546bf7b0f024
Péricles Jr <periclesmjr@hotmail.com>
2024-09-11 08:32:48 -0300

    adiciona index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// // Hello, world!
+// mensagem = "Hello, I!"
+
+// {
+//     const mensagem = "Hello, world!"
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem);
+// // console = objeto / log = função
+
+// arrays, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+    isChecked: (info) => {
+        console.log(info)
+    }
+}
+
+meta.isChecked(meta.value)
+
+// function // arrow function
+// const criarMeta = () => {}
+
+// function criarMeta() {}
