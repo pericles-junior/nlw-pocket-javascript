8e05c19c1d12b4ab29eb93713362abbc8e133502
Péricles Júnior <periclesmjr@hotmail.com>
2024-09-11 22:24:42 -0300

    deletar metas

--- index.js
+++ index.js
@@ -124,6 +124,31 @@ const metasAbertas = async () => {
   });
 };
 
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return {value: meta.value, checked: false}
+    })
+  const itensADeletar = await checkbox({
+    message: "Selecione item para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  });
+
+  if (itensADeletar.length == 0){
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itensADeletar.forEach((item) => {
+    metas = metas.filter((meta) => {
+        return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deletada(s) com sucesso!")
+  
+};
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -146,6 +171,10 @@ const start = async () => {
           value: "abertas",
         },
         {
+          name: "Deletar metas",
+          value: "deletar",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -165,6 +194,9 @@ const start = async () => {
         break;
       case "abertas":
         await metasAbertas();
+        break;
+      case "deletar":
+        await deletarMetas();
         break;
       case "sair":
         console.log("Até a próxima!");
--- todo.md
+++ todo.md
@@ -3,5 +3,5 @@
     - [x] Metas realizadas
     - [x] Metas abertas
 - [x] Marcar/desmarcar metas realizadas
-- Remover metas
+- [x] Remover metas
 - Sistema de mensagens
